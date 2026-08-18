ea9740d55bceefa6153b5e5710002a2d50b31408
Ahmed-programmer-1 <ahmed.programming.556@gmail.com>
2026-08-18 18:36:33 +0300

    تحديث كورس AAA12

--- AAA12.js
+++ AAA12.js
@@ -30,6 +30,11 @@ registerCourse("AAA12", {
       "title": "الحلقة 1",
       "link": "https://hanerix.com/stream/AHCliCGltwb8B_jBGdDZbw/kjhhiuahiuhgihdf/1787110366/67301418/master.m3u8",
       "description": ""
+    },
+    {
+      "title": "الحلقة 2",
+      "link": "https://hanerix.com/stream/AHCliCGltwb8B_jBGdDZbw/kjhhiuahiuhgihdf/1787110366/67301418/index-f3-v1-a1.m3u8",
+      "description": ""
     }
   ]
 });
